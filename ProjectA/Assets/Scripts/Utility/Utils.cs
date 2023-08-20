6a7385d7767d98647453cebdffc5c78f8ede4cca
SmugyCoding <smugy1011@gmail.com>
2023-08-20 21:11:18 +0800

    Add Utils Class
    新增Utils Class
    用於一些常見的數學計算
    和取得滑鼠座標

---
+++ ProjectA/Assets/Scripts/Utility/Utils.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+namespace Utility
+{
+    public static class Utils
+    {
+        public static Vector3 GetVectorFromAngle(int angle)
+        {
+            // angle = 0 -> 360
+            float angleRad = angle * (Mathf.PI / 180f);
+            return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+        }
+
+        public static Vector3 GetVectorFromAngle(float angle)
+        {
+            // angle = 0 -> 360
+            float angleRad = angle * (Mathf.PI / 180f);
+            return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+        }
+        public static float GetAngleFromVectorFloat(Vector3 dir)
+        {
+            dir = dir.normalized;
+            float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            if (n < 0) n += 360;
+
+            return n;
+        }
+        public static float GetAngleFromVectorFloatXZ(Vector3 dir)
+        {
+            dir = dir.normalized;
+            float n = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+            if (n < 0) n += 360;
+
+            return n;
+        }
+
+        public static int GetAngleFromVector(Vector3 dir)
+        {
+            dir = dir.normalized;
+            float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            if (n < 0) n += 360;
+            int angle = Mathf.RoundToInt(n);
+
+            return angle;
+        }
+
+        public static int GetAngleFromVector180(Vector3 dir)
+        {
+            dir = dir.normalized;
+            float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            int angle = Mathf.RoundToInt(n);
+
+            return angle;
+        }
+
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+        public static Vector3 GetDirToMouse(Vector3 fromPosition)
+        {
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            return (mouseWorldPosition - fromPosition).normalized;
+        }
+
+    }
+}
