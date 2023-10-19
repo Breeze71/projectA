5ede148c47dc3c77b8081b4f9cbb56ab9f4be682
SmugyCoding <smugy1011@gmail.com>
2023-10-20 01:40:55 +0800

    Revert "Unfinished Inventory System"
    This reverts commit 10da7a37ad0b3c2aff017c479c8eec562a232f04.

--- ProjectA/Assets/Scripts/InventorySystem/ItemScript/PickableItem.cs
+++ ProjectA/Assets/Scripts/InventorySystem/ItemScript/PickableItem.cs
@@ -1,32 +1,11 @@
 using InventorySystem;
 using UnityEngine;
 
-public class PickableItem : ItemIteractBase
+public class PickableItem :InteractableBase
 {
     [SerializeField] ItemObject item;
     [SerializeField] int amount;
 
-    protected override void OnTriggerEnter2D(Collider2D _other)
-    {
-        if (_other.gameObject.GetComponent<Inventory>()!=null)
-        {
-            if (icon != null)
-            {
-                icon.SetActive(true);
-            }
-        }
-    }
-    protected override void OnTriggerExit2D(Collider2D _other)
-    {
-        if (_other.gameObject.GetComponent<Inventory>() != null)
-        {
-            if (icon != null)
-            {
-                icon.SetActive(false);
-            }
-            canInteract = false;
-        }
-    }
     public override void Interact()
     {
         if(InputManager.Instance.IsInteractKeyDown())
